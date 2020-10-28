ceca65e1ca6188c531eee8c07b7738b08e762014
yatinghan <yatinghan.lisa@gmail.com>
2020-10-28 14:51:25 -0400

    updated c0 binary version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-wget http://c0.typesafety.net/dist/cc0-v0590-linux4.4.0-bin.tgz
+wget http://c0.typesafety.net/dist/cc0-v718-ubuntu18.04.5-bin.tgz
 tar -xvzf cc0-*.tgz 
-mv cc0 ~/.cc0/
+mv cc0-v718-ubuntu18.04.5-bin ~/.cc0/
 echo 'export PATH=$PATH:~/.cc0/bin' >> ~/.bashrc
 rm cc0-*.tgz
